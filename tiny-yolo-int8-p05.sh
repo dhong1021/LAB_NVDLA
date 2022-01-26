a646290dd7759a4b989ff3d8e43fbeb98a0a7649
dhong1021 <daehwadavidhong94@gmail.com>
2022-01-26 16:30:00 +0900

    test

---
+++ tiny-yolo-int8-p05.sh
@@ -0,0 +1,3 @@
+./darknet detector test coco.names yolov3-tiny.cfg yolov3-tiny-p05.weights -thresh 0.24 dog.jpg -quantized
+
+
---
+++ tiny-yolo.sh
@@ -0,0 +1,3 @@
+./darknet detector test coco.names yolov3-tiny.cfg yolov3-tiny-p005.weights -thresh 0.24 dog.jpg
+
+
